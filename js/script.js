3d790bfee024674446cb4cd666ddfd6e05a576b2
Alex <adrdra88@gmail.com>
2021-11-09 17:00:10 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const col = document.querySelector('div.col')
+
+for (let i = 1; i <=100; i++){
+    let box =`<div class='box'>${i}</li>`;
+    col.innerHTML += box;
+    
+    if(i % 3 == 0 && i % 5 == 0){
+        let box =`<div class='box fizz-buzz'>FizzBuzz</li>`;
+        col.innerHTML += box;
+    }
+    
+    else if(i % 3 == 0){
+        let box =`<div class='box fizz'>Fizz</li>`;
+        col.innerHTML += box;
+    }
+    
+    else if(i % 5 ==0){
+        let box =`<div class='box buzz'>Buzz</li>`;
+        col.innerHTML += box;
+
+    }
+    
+    
+    
+}
+
+
